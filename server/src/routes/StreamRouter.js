0fa9d7cd3f8134338b5ea6fc692236824cac8c64
Jason <jasonpconn@gmail.com>
2021-05-16 20:28:22 -0400

    Clean Up

--- server/src/routes/StreamRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import StreamController from '../controllers/StreamController.js'
-
-const StreamRouter = express.Router()
-
-StreamRouter.get('/', StreamController.index)
-
-export default StreamRouter
