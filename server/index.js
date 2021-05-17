47623fe5458ff7f7ad74ca986dc60816aaf23e88
Jason <jasonpconn@gmail.com>
2021-05-16 20:32:18 -0400

    what

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
 import http from 'http'
-<<<<<<< HEAD
-
-import app from './src/app.js'
-=======
 import dotenv from 'dotenv'
 
 import App from './src/App.js'
->>>>>>> d6b5381dd84f5ad2261d583de7f77028e73715cc
 import connect from './conf/db.js'
 
 dotenv.config()
@@ -14,9 +9,5 @@ const server = http.createServer(App)
 
 server.listen(3000, () => {
   console.log('Server started.')
-<<<<<<< HEAD
-  connect()
-=======
   connect(process.env.MONGO_URL)
->>>>>>> d6b5381dd84f5ad2261d583de7f77028e73715cc
 })
