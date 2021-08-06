5673b9b33569722b0a62f244aee60e11ddbb5f53
Jason <jasonpconn@gmail.com>
2021-08-06 19:29:03 -0400

    Added Progressbar, Scrubbing, + Episode Selection

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -9,8 +9,24 @@ export default function Player() {
   const [playing, setPlaying] = useState(false)
 
   const [currentEpisode, setCurrentEpisode] = useState(0)
+  const [currentProgress, setCurrentProgress] = useState(0)
 
+  const intervalRef = useRef()
   const audioRef = useRef(new Audio())
+
+  const { duration } = audioRef.current
+
+  const startInterval = () => {
+    clearInterval(intervalRef.current)
+
+    intervalRef.current = setInterval(() => {
+      const { ended, currentTime } = audioRef.current
+
+      ended
+        ? handleNext()
+        : setCurrentProgress(currentTime)
+    }, [1000])
+  }
 
   // Controller Functions
   const togglePlay = () => {
@@ -21,10 +37,12 @@ export default function Player() {
 
   const handlePlay = () => {
     audioRef.current.play()
+    startInterval()
   }
 
   const handlePause = () => {
     audioRef.current.pause()
+    clearInterval(intervalRef.current)
   }
 
   const handlePrev = () => {
@@ -37,6 +55,20 @@ export default function Player() {
     currentEpisode < episodes.length - 1
       ? setCurrentEpisode(currentEpisode + 1)
       : setCurrentEpisode(0)
+  }
+
+  const handleScrubbing = (event) => {
+    const { value } = event.target
+
+    clearInterval(intervalRef.current)
+    audioRef.current.currentTime = value
+    setCurrentProgress(audioRef.current.currentTime)
+
+    if (playing) handlePlay()
+  }
+
+  const selectEpisode = (key) => {
+    setCurrentEpisode(key)
   }
 
   // Fetch Episodes
@@ -58,6 +90,7 @@ export default function Player() {
     playing
       ? handlePlay()
       : handlePause()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [playing])
 
   // Handle Episode Changes
@@ -68,27 +101,43 @@ export default function Player() {
 
       handlePause()
       audioRef.current = new Audio(url)
+      setCurrentProgress(0)
       if (playing) handlePlay()
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentEpisode])
 
   return (
     <>
       {/* Progress Bar */}
-      
-      
+      <>
+        <input
+          type='range'
+          step='1'
+          min='0'
+          max={`${duration}`}
+          value={currentProgress}
+          onChange={(event) => handleScrubbing(event)}
+        />
+      </>
+
       {/* Player Controls */}
       <>
         <button onClick={() => handlePrev()}>prev</button>
         <button onClick={() => togglePlay()}>{playing ? 'pause' : 'play'}</button>
         <button onClick={() => handleNext()}>next</button>
+        
+        {/* Volume Controls */}
+
       </>
 
       {/* Episode List */}
       <ul>
         { loading
           ? 'loading...'
-          : episodes.map((episode, key) => (<li key={key}>{episode.title}</li>))
+          : episodes.map((episode, key) => (
+              <li key={key} onClick={() => selectEpisode(key)}>{episode.title}</li>)
+            )
         }
       </ul>
     </>
